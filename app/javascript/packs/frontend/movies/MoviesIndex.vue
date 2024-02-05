fa9e11a2d2813384258ef8e47a3470224ebbd0b8
kick250 <kicklobat@gmail.com>
2024-02-05 19:55:50 -0300

    terminar de configura vue

---
+++ app/javascript/packs/frontend/movies/MoviesIndex.vue
@@ -0,0 +1,23 @@
+<style>
+  .text-center {
+    text-align: center;
+  }
+</style>
+
+<template>
+  <div>
+    <h1 class="text-center">Filmes</h1>
+    <span>message: {{ message }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "hello world"
+    }
+  }
+}
+</script>
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,18 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const { DefinePlugin } = require('webpack')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.plugins.prepend(
+  'Define',
+  new DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  })
+)
+
 module.exports = environment
